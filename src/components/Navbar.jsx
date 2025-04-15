0b795cdcea337786271c3436afab0ab1a5271428
Mathias Kalanda <mathiauskalanda1@gmail.com>
2025-04-15 12:18:17 -0700

    added navbar iconmobilesize

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,31 @@
 import { Link } from "@tanstack/react-router";
 import React from "react";
+import { CiSearch } from "react-icons/ci";
+import { FaShoppingCart } from "react-icons/fa";
+import { HiOutlineBars3BottomRight } from "react-icons/hi2";
 // className = "[&.active]:font-bold";
 
 const Navbar = () => {
-  return <div className="bg-black">hello</div>;
+  return (
+    <div className="bg-black">
+      <div className="flex flex-row justify-between mx-3">
+        <div className="p-3">
+          <img src="/Logo.png" alt="" className="w-7" />
+        </div>
+        <div className="flex flex-row gap-4 p-3">
+          <button className="text-white">
+            <CiSearch size={24} />
+          </button>
+          <button className="text-white">
+            <FaShoppingCart size={24} />
+          </button>
+          <button className="text-white">
+            <HiOutlineBars3BottomRight size={24} />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

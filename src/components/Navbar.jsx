85b96c5841fb7ea7917c627d5449d2adfd7a16ce
Mathias Kalanda <mathiauskalanda1@gmail.com>
2025-04-15 10:47:16 -0700

    mk commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+import { Link } from "@tanstack/react-router";
+import React from "react";
+// className = "[&.active]:font-bold";
+
+const Navbar = () => {
+  return <div className="bg-black">hello</div>;
+};
+
+export default Navbar;

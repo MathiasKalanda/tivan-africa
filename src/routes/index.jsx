aaa7870fbcf7152d346dc6e3d06fa28b14b301f7
Mathias Kalanda <mathiauskalanda1@gmail.com>
2025-04-15 16:24:23 -0700

    added honey

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import HomePage from "../components/HomePage";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/"!</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }

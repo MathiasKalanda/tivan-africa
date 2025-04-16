cb5b5bbf6bd8bb6e6b392044ba34fbfc96e2193f
Mathias Kalanda <mathiauskalanda1@gmail.com>
2025-04-15 20:11:07 -0700

    sorted the gallery mb

--- src/components/HoneyGallery.jsx
+++ src/components/HoneyGallery.jsx
@@ -1,42 +1,51 @@
 import React from "react";
+import { FaArrowRight } from "react-icons/fa";
 
 const HoneyGallery = () => {
   return (
-    <div className="grid grid-cols-4 grid-rows-2 gap-4 p-6">
-      {/* Image 1 */}
-      <div className="col-span-2 row-span-2">
-        <img
-          src="/oil7.jpg"
-          alt="Oil"
-          className="w-full h-full object-cover rounded-xl shadow-lg"
-        />
+    <div className="pt-16">
+      {" "}
+      <div className="">
+        <h1 className="px-6 ">
+          <span className="text-3xl">Discover Our Enriched</span>
+          <br />{" "}
+          <div className="flex flex-row items-center gap-4">
+            {" "}
+            <div className="uppercase font-bold text-transparent bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text flex flex-row">
+              Varient Of Products
+            </div>{" "}
+            <FaArrowRight className="moving-arrow" />
+          </div>
+        </h1>
       </div>
+      <div className="grid grid-cols-4 grid-rows-2 gap-4 p-6">
+        {/* Image 1 */}
+        <div className="col-span-2 row-span-2">
+          <img
+            src="/oil7.jpg"
+            alt="Oil"
+            className="w-full h-full object-cover rounded-xl shadow-lg"
+          />
+        </div>
 
-      {/* Image 2 */}
-      <div className="col-span-2 row-span-1">
-        <img
-          src="/Organic.jpg"
-          alt="Africa"
-          className="w-full h-full object-cover rounded-xl shadow-lg"
-        />
+        {/* Image 2 */}
+        <div className="col-span-2 row-span-1">
+          <img
+            src="/Organic.jpg"
+            alt="Africa"
+            className="w-full h-full object-cover rounded-xl shadow-lg"
+          />
+        </div>
+
+        {/* Image 3 */}
+        <div className="col-span-2 row-span-1">
+          <img
+            src="/oil3.jpg"
+            alt="Butter"
+            className="w-full h-full object-cover rounded-xl shadow-lg"
+          />
+        </div>
       </div>
-
-      {/* Image 3 */}
-      <div className="col-span-2 row-span-1">
-        <img
-          src="/oil3.jpg"
-          alt="Butter"
-          className="w-full h-full object-cover rounded-xl shadow-lg"
-        />
-      </div>
-
-      {/* <div className="col-span-4 row-span-1">
-        <img
-          src="/fry.jpg"
-          alt="Honey"
-          className="w-full h-full object-cover rounded-xl shadow-lg"
-        />
-      </div> */}
     </div>
   );
 };

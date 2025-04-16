883e9326aa86578de995ec2a0cb3b4de29d143bd
Mathias Kalanda <mathiauskalanda1@gmail.com>
2025-04-16 00:17:59 -0700

    mobile navbar

--- src/components/MobileNav.jsx
+++ src/components/MobileNav.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { LiaArrowRightSolid } from "react-icons/lia";
 
 const MobileNav = ({ isOpen, MdClose, handleMenu }) => {
   return (
     <div>
-      <div className="bg-black text-white h-screen top-0 md:hidden flex fixed w-full">
+      <div className="bg-black text-white h-screen top-0 md:hidden flex fixed w-full flex-col items-center">
         <div className="flex flex-row justify-between mx-3 w-full p-3">
           {" "}
           <div className="px-5 py-3">
@@ -16,7 +17,40 @@ const MobileNav = ({ isOpen, MdClose, handleMenu }) => {
             <MdClose size={30} />
           </button>
         </div>
-        <hr className="text-green-900" />
+        <div className="self-start  h-full w-full px-10 py-6">
+          <ul className="text-white">
+            <li className="py-3 px-4 rounded-3xl border-1 hover:border-amber-500 my-5 flex flex-row items-center justify-between">
+              <span className="font-bold uppercase">Products</span>
+              <span className="moving-arrow text-green-800">
+                <LiaArrowRightSolid size={24} className="" />
+              </span>
+            </li>
+            <li className="py-3 px-4 rounded-3xl border-1 hover:border-amber-500 my-5 flex flex-row items-center justify-between">
+              <span className="font-bold uppercase">Orders</span>
+              <span className="moving-arrow text-green-800">
+                <LiaArrowRightSolid size={24} className="" />
+              </span>
+            </li>
+            <li className="py-3 px-4 rounded-3xl border-1 hover:border-amber-500 my-5 flex flex-row items-center justify-between">
+              <span className="uppercase font-bold">Policy</span>
+              <span className="moving-arrow text-green-800">
+                <LiaArrowRightSolid size={24} className="" />
+              </span>
+            </li>
+            <li className="py-3 px-4 rounded-3xl border-1 hover:border-amber-500 my-5 flex flex-row items-center justify-between">
+              <span className="font-bold uppercase">About Us</span>
+              <span className="moving-arrow text-green-800">
+                <LiaArrowRightSolid size={24} className="" />
+              </span>
+            </li>
+            <li className="py-3 px-4 rounded-3xl border-1 hover:border-amber-500 my-5 flex flex-row items-center justify-between cursor-pointer">
+              <span className="font-bold uppercase">Contact Us</span>
+              <span className="moving-arrow text-green-800">
+                <LiaArrowRightSolid size={24} className="" />
+              </span>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );

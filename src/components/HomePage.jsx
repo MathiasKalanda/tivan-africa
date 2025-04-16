f0785e7d57d2bf4edc9c52429a7fd33657da9c58
Mathias Kalanda <mathiauskalanda1@gmail.com>
2025-04-15 18:03:13 -0700

    added oil component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,41 +1,12 @@
 import React from "react";
+import Honey from "./Honey";
+import Oil from "./Oil";
 
 const HomePage = () => {
   return (
-    <div
-      style={{
-        background: "radial-gradient(circle,#435154,#1B252F,#05060B,#000000 )",
-
-        color: "white",
-      }}
-      className="pb-10"
-    >
-      {" "}
-      <div className="flex flex-col justify-center items-center ">
-        {" "}
-        <h1 className=" text-3xl md:text-4xl mt-24 font-bold uppercase text-transparent bg-gradient-to-r from-yellow-400 via-yellow-500 to-yellow-600 bg-clip-text">
-          Tivan Honey
-        </h1>
-        <p className="pt-2 font-bold mb-2 text-amber-700">
-          Natural and Pure Honey
-        </p>
-        <p className="text-wrap text-center px-20">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque Lorem,
-          ipsum dolor sit amet consectetur adipisicing elit. Neque
-        </p>
-        <div className="flex flex-row justify-between items-center gap-4 mt-4">
-          <button className="bg-amber-600 px-3 py-2 rounded-3xl text-black">
-            Know More
-          </button>
-          <button className="px-5 py-2 border-amber-600 border-1 rounded-3xl">
-            Buy
-          </button>
-        </div>
-        <img src="/nobgjar.png" alt="" className="w-80 " />
-        <div className="uppercase font-bold text-transparent bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text">
-          Taste the flavour beyond treasure
-        </div>
-      </div>
+    <div className="">
+      <Honey />
+      <Oil />
     </div>
   );
 };
---
+++ src/components/Oil.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+const Oil = () => {
+  return (
+    <div
+      style={{
+        background:
+          "linear-gradient(to bottom,#ffffff, #f9f6ef, #f0ead6, #e3dbc6)",
+
+        color: "white",
+      }}
+      className="pb-10"
+    >
+      {" "}
+      <div className="flex flex-col justify-center items-center ">
+        {" "}
+        <h1 className=" text-3xl md:text-4xl mt-24 font-bold uppercase text-transparent bg-gradient-to-r from-gray-800 via-black/30 via-black/40 to-black  bg-clip-text">
+          Tivan Oil
+        </h1>
+        <p className="pt-2 font-bold mb-2 text-amber-700">
+          Natural and Flavoured Oil
+        </p>
+        <img src="/oil2nobg.png" alt="" className="w-80 border-0" />
+        <div className="uppercase font-bold text-transparent bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text">
+          Taste the flavour beyond treasure
+        </div>
+        <p className="text-wrap text-center px-20 text-black">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque Lorem,
+          ipsum dolor sit amet consectetur adipisicing elit. Neque
+        </p>
+        <div className="flex flex-row justify-between items-center gap-4 mt-4">
+          <button className="bg-amber-600 px-3 py-2 rounded-3xl text-black font-bold">
+            Know More
+          </button>
+          <button className="px-5 py-2 text-black border-amber-600 border-1 rounded-3xl font-bold">
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Oil;

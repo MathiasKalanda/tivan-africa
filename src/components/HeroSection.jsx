6799aeaad4340973ac076e9de1ce11c2765b2aa1
Mathias Kalanda <mathiauskalanda1@gmail.com>
2025-04-23 18:13:18 -0700

    hero section touched

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,13 +1,16 @@
 import React from "react";
-import { FaArrowRight } from "react-icons/fa";
+import { FaArrowRight, FaHome, FaOpencart, FaWhatsapp } from "react-icons/fa";
 import { IoMdMore } from "react-icons/io";
 import { IoChatbubbleOutline } from "react-icons/io5";
 import { PiHeartStraight } from "react-icons/pi";
-// import honeyVideo from "../public/honeyvideo.mp4";
+
 const HeroSection = () => {
+  const phoneNumber = "256776978176";
+  const message = "Hello, I’m interested in your products!";
+  const whatsappUrl = `https://wa.me/${phoneNumber}?text=${encodeURIComponent(message)}`;
   return (
     <div className="mt-10">
-      <div className="h-screen  pt-10 flex flex-row  items-end justify-between pb-20 relative">
+      <div className="h-screen  pt-10 flex flex-col  items-center justify-between pb-20 relative">
         <video
           className="fixed top-0 left-0 w-full h-full object-cover z-[-1] mt-10"
           autoPlay
@@ -17,40 +20,48 @@ const HeroSection = () => {
           <source src="/honeyvideo.mp4" type="video/mp4" />
           Your browser does not support the video tag.
         </video>
-        <div className="flex flex-col gap-10 w-96 mx-10">
+        <div className="self-center pt-40  font-bold text-4xl bg-gradient-to-r from-green-700 via-green-400 to-lime-300 bg-clip-text text-transparent">
+          Tivan Organic Products
+        </div>
+        <p className="text-center font-bold text-lg">
+          Organic Butter. Golden Honey. Natural Oils.
+        </p>
+        <span className="font-bold underline itali mb-5">
+          Rooted in nature. Crafted with care.
+        </span>
+        <div className="flex flex-col gap-5 w-96 mx-10">
           <div className="flex flex-row items-center gap-5">
-            <div className="w-10 h-10 rounded-full bg-red-600"></div>
-            <div className="">jewelry_prices</div>
-            <div className="border-2 border-white px-5 py-1 rounded-lg">
-              follow
+            <div className="bg-amber-700/35 p-2 rounded-full">
+              {" "}
+              <img src="/Logo.png" alt="" className="w-10" />
+            </div>
+
+            <div className="flex flex-row items-center gap-2 ">
+              <span className="font-bold underline">Contact Us Direct</span>{" "}
+              <FaArrowRight className="moving-arrow " />
+            </div>
+            <div className="border-2 border-green-600 px-5 py-1 rounded-lg">
+              <button onClick={() => window.open(whatsappUrl, "_blank")}>
+                <FaWhatsapp size={24} className="text-green-600" />
+              </button>
             </div>
           </div>
           <div className="bg-black/25 p-5 rounded-md flex flex-col gap-5">
             <div className="">
-              <div className="w-10 flex flex-row">
-                <div className="w-20 h-10 bg-gray-500 rounded-lg">hey</div>
-                <div className="text-nowrap">50K followers.493 posts</div>
+              <div className="w-10 flex flex-row  items-center gap-3">
+                <div className="text-nowrap text-xl font-bold text-black uppercase">
+                  Honey <span className="font-black text-green-800">|</span> Oil{" "}
+                  <span className="font-black text-green-800">|</span>{" "}
+                  Butter{" "}
+                </div>
               </div>
             </div>
             <div className="bg-amber-700 flex flex-row justify-between items-center rounded-md py-2 px-2">
               <span className="capitalize font-bold">
-                visit instagram profile
+                view Tivan Organic Products
               </span>
               <FaArrowRight />
             </div>
-          </div>
-        </div>
-        <div className="flex flex-col gap-4 mr-5">
-          <div className="flex flex-col items-center justify-center">
-            <PiHeartStraight />
-            <h1 className="">375</h1>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <IoChatbubbleOutline />
-            <h1 className="">7</h1>
-          </div>
-          <div className="flex flex-col items-center justify-center">
-            <IoMdMore />
           </div>
         </div>
       </div>

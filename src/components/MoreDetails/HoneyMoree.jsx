6312f2a1e4f41528b227377624bebcd76a313e2e
Mathias Kalanda <mathiauskalanda1@gmail.com>
2025-05-23 12:07:01 -0700

    commit mk

--- src/components/MoreDetails/HoneyMoree.jsx
+++ src/components/MoreDetails/HoneyMoree.jsx
@@ -8,7 +8,7 @@ const HoneyMoree = () => {
       </div>
       <div class="relative grid min-h-screen grid-cols-[1fr_2.5rem_auto_2.5rem_1fr] grid-rows-[1fr_1px_auto_1px_1fr] bg-white [--pattern-fg:var(--color-gray-950)]/5 dark:bg-gray-950 dark:[--pattern-fg:var(--color-white)]/10">
         <div class="col-start-3 row-start-3 flex max-w-lg flex-col bg-gray-100 p-2 dark:bg-white/10">
-          <div class="rounded-xl bg-white p-10 text-sm/7 text-gray-700 dark:bg-gray-950 dark:text-gray-300">
+          <div class="rounded-xl bg-white p-2 md:p-10 text-sm/7 text-gray-700 dark:bg-gray-950 dark:text-gray-300">
             <img
               src="/img/logo.svg"
               class="mb-11.5 h-6 dark:hidden"
